b739a39e0e6db5bed20e88f26fbe0bc5e0490d83
damccull <401104+damccull@users.noreply.github.com>
2023-11-27 22:26:45 -1000

    Refactor to allow features to select shuttle or standalone

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,52 @@ use cch_23::{
     telemetry,
 };
 
-#[cfg(shuttle)]
+#[cfg(feature = "shuttle")]
 use shuttle_actix_web::ShuttleActixWeb;
 
-/// [`main`] return type when `shuttle` feature is active
-#[cfg(shuttle)]
-type MainReturn = ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static>;
-
-/// [`main`] return type when `shuttle` feature is disabled
-#[cfg(not(feature="shuttle"))]
-type MainReturn = Result<(), ()>;
-
-// If the 'shuttle' features is enabled, run this in shuttle's actix_web runner,
-// otherwise use tokio's runner
-#[cfg_attr(feature="shuttle", shuttle_runtime::main)]
-#[cfg_attr(not(feature="shuttle"), tokio::main)]
+// If the 'shuttle' features is enabled, run this in shuttle's actix_web runner.
+#[cfg(feature = "shuttle")]
+#[shuttle_runtime::main]
 #[tracing::instrument]
-async fn main() -> MainReturn {
-    // Set up tracing
-    let subscriber =
-        telemetry::get_subscriber("shuttle-cch".into(), "info".into(), std::io::stdout);
-    telemetry::init_subscriber(subscriber);
+async fn shuttle_main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static>
+{
+    setup_tracing();
 
     tracing::info!("Tracing enabled.");
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(home).service(health_check);
+        configure_routes(cfg);
     };
     tracing::info!("Services online.");
 
-    #[cfg(feature="shuttle")]
-    {
-        Ok(config.into())
-    }
-    #[cfg(not(feature="shuttle"))]
-    {
-        println!("No shuttle");
-        Ok(())
-    }
+    Ok(config.into())
+}
+
+// If the 'shuttle' feature is disabled, run this in actix_web's native runner.
+#[cfg(not(feature = "shuttle"))]
+#[actix_web::main]
+#[tracing::instrument]
+async fn main() -> std::io::Result<()> {
+    use actix_web::{App, HttpServer};
+    setup_tracing();
+    tracing::info!("Tracing enabled.");
+
+    let config = move |cfg: &mut ServiceConfig| {
+        configure_routes(cfg);
+    };
+    HttpServer::new(move || App::new().configure(config))
+        .bind(("127.0.0.1", 8000))?
+        .run()
+        .await
+}
+
+fn setup_tracing() {
+    // Set up tracing
+    let subscriber =
+        telemetry::get_subscriber("shuttle-cch".into(), "info".into(), std::io::stdout);
+    telemetry::init_subscriber(subscriber);
+}
+
+fn configure_routes(cfg: &mut ServiceConfig) {
+    cfg.service(home).service(health_check);
 }
